dbcc8f133906f269e48978cc8ac1fc4b86a8558c
Nick Moresco <nick.moresco@gmail.com>
2020-12-02 13:24:06 -0800

    extract out a common function

--- src/day2.rs
+++ src/day2.rs
@@ -3,16 +3,11 @@ use regex::Regex;
 pub fn solve_floor() {
     let mut num_valid = 0;
 
-    let regex = Regex::new("^(\\d+)-(\\d+) ([a-zA-Z]): ([a-zA-Z]+$)").unwrap();
     let data = include_str!("../resources/2-1.txt").lines();
     for line in data {
-        let captures = regex.captures(line).unwrap();
-        let min_occurs = captures.get(1).unwrap().as_str().parse().unwrap();
-        let max_occurs = captures.get(2).unwrap().as_str().parse().unwrap();
-        let rule = captures.get(3).unwrap().as_str().chars().next().unwrap();
-        let password = captures.get(4).unwrap().as_str().to_string();
+        let (min_occurs, max_occurs, rule, password) = parse_password_info(line);
 
-        let count = password.matches(rule).count();
+        let count = password.matches(rule as char).count();
         if count >= min_occurs && count <= max_occurs {
             num_valid += 1;
         }
@@ -24,14 +19,10 @@ pub fn solve_floor() {
 pub fn solve_basement() {
     let mut num_valid = 0;
 
-    let regex = Regex::new("^(\\d+)-(\\d+) ([a-zA-Z]): ([a-zA-Z]+$)").unwrap();
     let data = include_str!("../resources/2-1.txt").lines();
     for line in data {
-        let captures = regex.captures(line).unwrap();
-        let first_index: usize = captures.get(1).unwrap().as_str().parse().unwrap();
-        let second_index: usize = captures.get(2).unwrap().as_str().parse().unwrap();
-        let rule = captures.get(3).unwrap().as_str().as_bytes()[0];
-        let password = captures.get(4).unwrap().as_str().to_string().as_bytes();
+        let (first_index, second_index, rule, password) = parse_password_info(line);
+        let password = password.as_bytes();
 
         if (password[first_index - 1] == rule && password[second_index - 1] != rule)
             || (password[first_index - 1] != rule && password[second_index - 1] == rule) {
@@ -40,4 +31,15 @@ pub fn solve_basement() {
     }
 
     println!("{}", num_valid);
+}
+
+fn parse_password_info(line: &str) -> (usize, usize, u8, String) {
+    let regex = Regex::new("^(\\d+)-(\\d+) ([a-zA-Z]): ([a-zA-Z]+$)").unwrap();
+
+    let captures = regex.captures(line).unwrap();
+    let first_index: usize = captures.get(1).unwrap().as_str().parse().unwrap();
+    let second_index: usize = captures.get(2).unwrap().as_str().parse().unwrap();
+    let rule = captures.get(3).unwrap().as_str().as_bytes()[0];
+    let password = captures.get(4).unwrap().as_str().to_string();
+    (first_index, second_index, rule, password)
 }
