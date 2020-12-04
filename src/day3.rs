314d59ea542a982b77c06295f59dba1bfd07053f
Nick Moresco <nick.moresco@gmail.com>
2020-12-03 20:43:19 -0800

    Clean up some types, improve the fold function

--- src/day3.rs
+++ src/day3.rs
@@ -12,12 +12,15 @@ pub fn solve_basement() {
     let array = create_board(data);
 
     let slopes: Vec<(usize, usize)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let num_trees = slopes.iter().fold(1, |acc, (x, y)| acc * traverse_slope(&array, *x, *y));
+    let num_trees: u64 = slopes
+        .iter()
+        .map(|(x, y)| traverse_slope(&array, *x, *y))
+        .product();
 
     println!("{}", num_trees);
 }
 
-fn traverse_slope(array: &Vec<Vec<char>>, x_steps: usize, y_steps: usize) -> u64 {
+fn traverse_slope(array: &[Vec<char>], x_steps: usize, y_steps: usize) -> u64 {
     let mut count = 0;
     let mut x: usize = 0;
     let mut y: usize = 0;
--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,11 @@
 pub fn create_board(data: &str) -> Vec<Vec<char>> {
     data.trim()
-        .split("\n")
+        .split('\n')
         .map(|row| row.trim().chars().collect())
         .collect()
 }
 
-pub fn print_board(board: &Vec<Vec<char>>) {
+pub fn print_board(board: &[Vec<char>]) {
     for row in board{
         for col in row {
             print!("{}", col);
